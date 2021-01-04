4d06c6574cbcb0f97a327bef05ff087837fddbf5
Adrian <colid.bondzio@gmail.com>
2021-01-04 18:29:33 +0100

    removing useless code

--- setup.py
+++ setup.py
@@ -14,21 +14,14 @@ def portfolio(page_name):
     return render_template(page_name)
 
 
-def write_to_database(data):
-    with open('database.txt', mode='a') as database:
-        email = data['e-mail']
-        subject = data['subject']
-        message = data['message']
-        file = database.write(f'\n{email},{subject},{message}')
-
 def write_to_csv(data):
     with open('database.csv', newline="", mode='a') as database2:
         email = data['e-mail']
         subject = data['subject']
         message = data['message']
-        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer = csv.writer(database2, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email, subject, message])
-
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
@@ -42,31 +35,3 @@ def submit_form():
             return 'Did not save to database.'
     else:
         return 'oooops, please fix it'
-
-
-# above is simplified method
-# now theres a problem with home.html and index.html
-# @app.route('/work.html')
-# def work():
-#     return render_template('work.html')
-
-
-# @app.route('/works.html')  # what is the link in the browser
-# def works():
-#     return render_template('works.html')
-
-
-# @app.route('/about.html')
-# def about_me():
-#     return render_template('about.html')
-
-
-# @app.route('/contact.html')
-# def contact():
-#     return render_template('contact.html')
-
-
-# dont need this
-# @app.route('/components.html')
-# def components():
-#   return render_template('components.html')
